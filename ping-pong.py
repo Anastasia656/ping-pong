26330e35a306b729e2b7af4cbceffde552d7b3ca
Anastasia656 <anastasiiapopova1223@rambler.ru>
2025-06-24 14:21:00 +0300

    Add files via upload

--- ping-pong.py
+++ ping-pong.py
@@ -3,14 +3,14 @@ H = 500
 W = 700
 Game = True
 clock = pygame.time.Clock()
-window = pygame.display.set_mode((W,H))
+window = pygame.display.set_mode((W,H))#содаём окно
 ball_img = pygame.Surface((30,30))
 player_img = pygame.Surface((30,60))
-class Base():
+
+class Base():#базовый класс
     def __init__(self,x,y,img,speed):
         self.hitbox = img.get_rect(center = (x,y))
         self.img = img
-        
         self.speed = speed
 
 class Ball(Base):
@@ -18,7 +18,7 @@ class Ball(Base):
         super().__init__(x, y, img, speed)
         self.speed_x = speed
         self.speed_y = speed
-    def move(self):
+    def move(self):#функция чтобы мяч двигался
         self.hitbox.x += self.speed_x
         self.hitbox.y += self.speed_y
         if self.hitbox.right > W:
@@ -32,14 +32,13 @@ class Ball(Base):
 ball = Ball(W//2,H//2,ball_img,15)
 
 
-
 while Game:
     window.fill((250,250,200))
-    event_list = pygame.event.get()
+    event_list = pygame.event.get()#подключаем события
     for event in event_list: 
         if event.type == pygame.QUIT: 
             Game = False
-    window.blit(ball.img,ball.hitbox)
-    ball.move()
+    window.blit(ball.img,ball.hitbox)#наносим мяч на экран
+    ball.move()#заставляем мяч двигаться
     pygame.display.update() 
     clock.tick(30)
